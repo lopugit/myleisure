bfb9391d4e2d307ff57ce4bb31f7b37ee48ad5c8
nikolaj <lopudesigns@gmail.com>
2021-08-05 19:05:13 +1000

    dev

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,8 +4,17 @@ module.exports = {
    * http://pm2.keymetrics.io/docs/usage/application-declaration/
    */
   apps : [
-
-    // First application
+    {
+      name      : 'dev-myleisure',
+      script    : 'app.js',
+      env: {
+        COMMON_VARIABLE: 'true'
+      },
+      env_production : {
+        NODE_ENV: 'production'
+      },
+      watch	: ["app.js","models","routes"]
+    },
     {
       name      : 'prod-myleisure',
       script    : 'app.js',
@@ -17,37 +26,5 @@ module.exports = {
       },
       watch	: ["app.js","models","routes"]
     },
-
-    // Second application
-    //{
-      //name      : 'WEB',
-      //script    : 'web.js'
-    //}
   ],
-
-  /**
-   * Deployment section
-   * http://pm2.keymetrics.io/docs/usage/deployment/
-   */
-  deploy : {
-    production : {
-      user : 'node',
-      host : '212.83.163.1',
-      ref  : 'origin/master',
-      repo : 'git@github.com:lopudesigns/myleisure.com.au.git',
-      path : '/var/www/production',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
-    },
-    dev : {
-      user : 'node',
-      host : '212.83.163.1',
-      ref  : 'origin/master',
-      repo : 'git@github.com:repo.git',
-      path : '/var/www/development',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env dev',
-      env  : {
-        NODE_ENV: 'dev'
-      }
-    }
-  }
 };
